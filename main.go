86274bb8f37dbbe9c793b820a232cb32004d4e06
Ruslan Kairanov <m38r@mail.ru>
2026-04-03 16:34:41 +0500

    start bot

--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
 func main() {
-	
+
+	token := os.Getenv("tg_token")
+
+	if token == "" {
+		log.Fatal("tg_bot empty")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(token)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("authorized as @%s", bot.Self.UserName)
+
+	UpdateConfig := tgbotapi.NewUpdate(0)
+	UpdateConfig.Timeout = 30
+
+	updates := bot.GetUpdatesChan(UpdateConfig)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf(
+			"message from %s: %s",
+			update.Message.From.UserName,
+			update.Message.Text,
+		)
+
+		replyText := "Я получил сообщение:" + update.Message.Text
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, replyText)
+
+		_, err := bot.Send(msg)
+		if err != nil {
+			log.Println("send message error:", err)
+		}
+
+	}
 }
